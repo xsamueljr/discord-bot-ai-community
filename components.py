50e8b7980fc58b247810c1a20f250d991aa53d78
xsamueljr <samueljiro.alfa@gmail.com>
2024-03-26 22:00:41 +0100

    LOL

--- components.py
+++ components.py
@@ -3,20 +3,22 @@ from enum import Enum
 import discord
 
 
+SERVER_ID: int = 1216501295033225227
+
+MAILBOX_ID: int = 1221907546177011762
+
 class Category(Enum):
+    category1 = "category1"
+    category2 = "category2"
+    """
     basics = "basics"
     medium = "medium"
     advanced = "advanced"
     offtopic = "offtopic"
+    """
 
-
-class ApportationView(discord.ui.View):
-    @discord.ui.button(label="Aprobar", style=discord.ButtonStyle.green)
-    async def approve(self, button: discord.ui.Button, interaction: discord.Interaction):
-        await interaction.message.delete()
-        await interaction.channel.send("Aprobado!")
-    
-    @discord.ui.button(label="Rechazar", style=discord.ButtonStyle.red)
-    async def reject(self, button: discord.ui.Button, interaction: discord.Interaction):
-        await interaction.message.delete()
-        await interaction.channel.send("Rechazado!")
+    def get_channel_id(self) -> int:
+        return {
+            "category1": 1221907587826712738,
+            "category2": 1221907603932709006
+        }[self.value]
--- main.py
+++ main.py
@@ -1,45 +1,112 @@
 import os
+from asyncio import run
 
 from dotenv import load_dotenv
-import discord
+from discord import (
+    User,
+    Member,
+    Bot,
+    Intents,
+    Guild,
+    Message,
+    Embed
+)
+import discord.utils
 
-from components import Category, ApportationView
-from constants import SERVER_ID, MAILBOX_CHANNEL
+from components import Category
+from components import SERVER_ID, MAILBOX_ID
+from validation import is_valid_url
 
 load_dotenv()
 
-bot = discord.Bot()
+bot: Bot = Bot()
+
+rejection_messages: list[Message] = []
+
+class ApportationView(discord.ui.View):
+    @discord.ui.button(label="Aprobar", style=discord.ButtonStyle.green)
+    async def approve(self, button: discord.ui.Button, interaction: discord.Interaction):
+        embed = interaction.message.embeds[0]
+        author = discord.utils.get(interaction.guild.members, mention=embed.fields[0].value)
+        assert author is not None, f"{embed.fields[0].value}"
+        category = embed.fields[1].value
+        channel = category1_channel if category == Category.category1.value else category2_channel
+
+        embed.remove_field(1)
+        
+        await interaction.message.delete()
+        await channel.send(embed=embed)
+        await author.add_roles(contributor_role, reason="Aportación aprobada")
+
+
+    @discord.ui.button(label="Rechazar", style=discord.ButtonStyle.red)
+    async def reject(self, button: discord.ui.Button, interaction: discord.Interaction):
+        embed = interaction.message.embeds[0]
+        author = discord.utils.get(interaction.guild.members, mention=embed.fields[0].value)
+        assert author is not None, f"{embed.fields[0].value}"
+
+        embed.remove_field(0)
+        embed.title = None
+        msg = (
+            "¡Hola!",
+            "Lamentablemente, tu aportación fue rechazada. Contacta con algún administrador si crees que fue un error, o quieres saber el motivo.",
+            "Éste mensaje se borrará en 20 segundos. Sácale captura si quieres."
+            "Te adjunto la aportación para que puedas revisarla. ¡Un saludo!"
+        )
+        
+        message = await author.send("\n\n".join(msg), embed=embed, delete_after=20)
+        rejection_messages.append(message)
+        await interaction.message.delete()
 
 
 @bot.event
-async def on_ready():
+async def on_ready() -> None:
+    global mailbox_channel, category1_channel, category2_channel
+    mailbox_channel = bot.get_channel(MAILBOX_ID)
+    category1_channel = bot.get_channel(Category.category1.get_channel_id())
+    category2_channel = bot.get_channel(Category.category2.get_channel_id())
+
+    global contributor_role
+    contributor_role = bot.get_guild(SERVER_ID).get_role(1222214686695624814)
+    
     print("Ready!")
 
 
 @bot.event
-async def on_guild_join(guild: discord.Guild):
+async def on_guild_join(guild: Guild) -> None:
     if guild.id != SERVER_ID:
-        # Exit the server
         await guild.leave()
 
 
 @bot.slash_command()
-async def hello(ctx, name: str = None):
+async def hello(ctx, name: str = None) -> None:
     name = name or ctx.author.name
     await ctx.respond(f"Hello {name}!", view=ApportationView())
 
 
 @bot.slash_command(name="aportar")
-async def send_apportation(ctx, category: Category, link: str):
-    message = discord.Embed(
-        title="¡Nueva aportación!"
-    )
+async def send_apportation(ctx, category: Category, link: str, description: str) -> None:
+    if len(description) not in range(10, 341):
+        await ctx.send_response("La descripción debe tener entre 10 y 340 caracteres.", ephemeral=True)
+        return
 
-    message.add_field(name="Autor", value=ctx.author.name)
-    message.add_field(name="Categoría", value=category.name)
-    message.add_field(name="Link", value=link)
+    if not link.startswith(("https://", "http://")):
+        link = f"https://{link}"
 
-    await discord.channel.Channel(id=MAILBOX_CHANNEL).send(embed=message)
+    
+    if not is_valid_url(link):
+        await ctx.send_response("El link no es válido.", ephemeral=True)
+        return
+
+    message = Embed(title="¡Nueva aportación!")
+
+    message.add_field(name="Autor", value=ctx.author.mention, inline=False)
+    message.add_field(name="Categoría", value=category.name, inline=False)
+    message.add_field(name="Link", value=link, inline=False)
+    message.add_field(name="Descripción", value=description, inline=False)
+
+    await mailbox_channel.send(embed=message, view=ApportationView())
+    await ctx.send_response("Aportación enviada con éxito.", ephemeral=True)
 
 
 bot.run(os.getenv("BOT_TOKEN"))
